17f066c4f8ee9df3e37282bb60f43fb43e65cd2f
takazcao <taka@gmail.com>
2025-06-01 13:41:06 +0700

    update code for 6 june

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,9 @@
 import Image from "next/image";
-import Hello from "./components/hello";
 
 export default function Home() {
   return (
     <>
       <h1 className="font-bold text-xl">Hello THIS is INTER </h1>;
-      <Hello />
     </>
   );
 }
--- app/dashboard/users/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const page = () => {
-  return <div>user page details</div>;
-};
-
-export default page;
